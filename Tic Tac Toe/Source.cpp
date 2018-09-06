a4db6f9be2dc111ebbfe1a22b74675450ab973f4
Jordon Cotton <s188044@students.aie.edu>
2018-09-06 16:42:20 -0500

    functions

--- Tic Tac Toe/Source.cpp
+++ Tic Tac Toe/Source.cpp
@@ -2,10 +2,8 @@
 
 char square[10] = { '0','1','2','3','4','5','6','7','8','9' };
 
-int checkwin();
+int win();
 void board();
-int X;
-int O;
 
 int main()
 {
@@ -21,4 +19,48 @@ int main()
 		std::cin >> choice;
 
 		mark = (player == 1) ? 'X' : 'O';
-	}
+
+		if (choice == 1 && square[1] == '1')
+
+			square[1] = mark;
+		else if (choice == 2 && square[2] == '2')
+
+			square[2] = mark;
+		else if (choice == 3 && square[3] == '3')
+
+			square[3] = mark;
+		else if (choice == 4 && square[4] == '4')
+
+			square[4] = mark;
+		else if (choice == 5 && square[5] == '5')
+
+			square[5] = mark;
+		else if (choice == 6 && square[6] == '6')
+
+			square[6] = mark;
+		else if (choice == 7 && square[7] == '7')
+
+			square[7] = mark;
+		else if (choice == 8 && square[8] == '8')
+
+			square[8] = mark;
+		else if (choice == 9 && square[9] == '9')
+
+			square[9] = mark;
+		else
+		{
+			std::cout << "Invalid move ";
+
+			player--;
+			std::cin.ignore();
+			std::cin.get();
+		}
+
+			int win()
+		{
+			return 0;
+		}
+
+		void board()
+		{
+		}
