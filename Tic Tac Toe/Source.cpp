7d30b1bf4837a5e0cc014d195a0db090b26ade51
jordoncotton <42358762+jordoncotton@users.noreply.github.com>
2018-09-09 20:10:19 -0500

    Update Source.cpp
    tictactoe

--- Tic Tac Toe/Source.cpp
+++ Tic Tac Toe/Source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-char square[10] = { '0','1','2','3','4','5','6','7','8','9' };
+char square[3][3] = {{ '1','2','3'}{'4','5','6'}{'7','8','9'} };
 int nums[10];
 int win();
 void board();
